c604b2045b2b079d66503f7d8173f84c2ad6326d
MichaelR <spider747@gmail.com>
2021-05-28 20:28:41 +0300

    Add CSS style sheet

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Task Manager</h1>
+
+      <div>
+        <ul>
+          <li>Item1</li>
+          <li>Item2</li>
+          <li>Item3</li>
+        </ul>
+      </div>
+
     </div>
   );
 }
