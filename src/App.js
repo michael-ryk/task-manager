218de186620bca75d42563e2725c0b63dc62f8c7
MichaelR <spider747@gmail.com>
2021-05-30 22:47:01 +0300

    Add input field

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Task Manager</h1>
+    <div className="main-container">
+    
+      <div className="heading">
+        <h1>Task Manager</h1>
+      </div>
 
-      <div>
+      <div className="form">
+        <input type="text"/>
+        <button>Add</button>
+      </div>
+
+      <div className="ItemsList">
         <ul>
           <li>Item1</li>
           <li>Item2</li>
